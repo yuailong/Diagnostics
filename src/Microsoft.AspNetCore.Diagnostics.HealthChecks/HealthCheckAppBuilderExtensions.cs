a60e0a69abeb39f8c5f09f40516181dab7ea8e76
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2017-10-11 10:03:28 -0700

    some pr feedback

--- src/Microsoft.AspNetCore.Diagnostics.HealthChecks/HealthCheckAppBuilderExtensions.cs
+++ src/Microsoft.AspNetCore.Diagnostics.HealthChecks/HealthCheckAppBuilderExtensions.cs
@@ -2,14 +2,21 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.AspNetCore.Http;
 
 namespace Microsoft.AspNetCore.Builder
 {
     public static class HealthCheckAppBuilderExtensions
     {
+        /// <summary>
+        /// Adds a middleware that provides a REST API for requesting health check status.
+        /// </summary>
+        /// <param name="applicationBuilder">The <see cref="IApplicationBuilder"/></param>
+        /// <param name="path">The path on which to provide the API</param>
+        /// <returns></returns>
         public static IApplicationBuilder UseHealthChecks(this IApplicationBuilder applicationBuilder, PathString path)
         {
+            applicationBuilder = applicationBuilder ?? throw new ArgumentNullException(nameof(applicationBuilder));
+
             return applicationBuilder.UseMiddleware<HealthCheckMiddleware>(new HealthCheckOptions()
             {
                 Path = path
--- src/Microsoft.Extensions.Diagnostics.HealthChecks.Abstractions/HealthCheckStatus.cs
+++ src/Microsoft.Extensions.Diagnostics.HealthChecks.Abstractions/HealthCheckStatus.cs
@@ -24,17 +24,17 @@ namespace Microsoft.Extensions.Diagnostics.HealthChecks
         Failed = 1,
 
         /// <summary>
-        /// Indicates that the health check determined that the component was unhealthy
+        /// Indicates that the health check determined that the component was unhealthy.
         /// </summary>
         Unhealthy = 2,
 
         /// <summary>
-        /// Indicates that the health check determined that the component was in a degraded state
+        /// Indicates that the health check determined that the component was in a degraded state.
         /// </summary>
         Degraded = 3,
 
         /// <summary>
-        /// Indicates that the health check determined that the component was healthy
+        /// Indicates that the health check determined that the component was healthy.
         /// </summary>
         Healthy = 4,
     }
