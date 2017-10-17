7295171aeb4cd989d2385d909a15a3ad97257d74
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-10-17 16:58:51 -0700

    doc comments

--- src/Microsoft.AspNetCore.Diagnostics.HealthChecks/HealthCheckAppBuilderExtensions.cs
+++ src/Microsoft.AspNetCore.Diagnostics.HealthChecks/HealthCheckAppBuilderExtensions.cs
@@ -8,19 +8,22 @@ using Microsoft.Extensions.Options;
 
 namespace Microsoft.AspNetCore.Builder
 {
+    /// <summary>
+    /// <see cref="IApplicationBuilder"/> extension methods for the <see cref="HealthCheckMiddleware"/>.
+    /// </summary>
     public static class HealthCheckAppBuilderExtensions
     {
         /// <summary>
         /// Adds a middleware that provides a REST API for requesting health check status.
         /// </summary>
-        /// <param name="applicationBuilder">The <see cref="IApplicationBuilder"/></param>
-        /// <param name="path">The path on which to provide the API</param>
-        /// <returns></returns>
-        public static IApplicationBuilder UseHealthChecks(this IApplicationBuilder applicationBuilder, PathString path)
+        /// <param name="app">The <see cref="IApplicationBuilder"/>.</param>
+        /// <param name="path">The path on which to provide the API.</param>
+        /// <returns>A reference to the <paramref name="app"/> after the operation has completed.</returns>
+        public static IApplicationBuilder UseHealthChecks(this IApplicationBuilder app, PathString path)
         {
-            applicationBuilder = applicationBuilder ?? throw new ArgumentNullException(nameof(applicationBuilder));
+            app = app ?? throw new ArgumentNullException(nameof(app));
 
-            return applicationBuilder.UseMiddleware<HealthCheckMiddleware>(Options.Create(new HealthCheckOptions()
+            return app.UseMiddleware<HealthCheckMiddleware>(Options.Create(new HealthCheckOptions()
             {
                 Path = path
             }));
